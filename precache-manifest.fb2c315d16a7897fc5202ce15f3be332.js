3d856f9a63750f34812898e2677b407edabdb782
Jha <AkumaJha@ad.harman.com>
2020-05-28 11:29:58 +0530

    Updates

---
+++ precache-manifest.fb2c315d16a7897fc5202ce15f3be332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a51f8156ecd562ddb920330a9952cea",
+    "url": "/Corona_Tracker_App/index.html"
+  },
+  {
+    "revision": "ee06515674e2e2d69ceb",
+    "url": "/Corona_Tracker_App/static/css/main.6eb20878.chunk.css"
+  },
+  {
+    "revision": "284fd1b6c798d0daf76f",
+    "url": "/Corona_Tracker_App/static/js/2.31588064.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona_Tracker_App/static/js/2.31588064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee06515674e2e2d69ceb",
+    "url": "/Corona_Tracker_App/static/js/main.bd94a3a5.chunk.js"
+  },
+  {
+    "revision": "1ad3bcd938b335f69c3c",
+    "url": "/Corona_Tracker_App/static/js/runtime-main.c76def0c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona_Tracker_App/static/media/image.d7265326.png"
+  }
+]);
