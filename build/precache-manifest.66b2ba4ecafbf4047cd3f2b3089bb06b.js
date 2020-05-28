017ddce3d1a593e7f379770b6542517a4cfdeade
Jha <AkumaJha@ad.harman.com>
2020-05-28 11:26:05 +0530

    Corona Tracker App deploy

---
+++ build/precache-manifest.66b2ba4ecafbf4047cd3f2b3089bb06b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bb6417d82372be1f6c6eae9f07301a",
+    "url": "/project_corona_tracker/index.html"
+  },
+  {
+    "revision": "e1643cf56baa55d0551f",
+    "url": "/project_corona_tracker/static/css/main.9e8d895b.chunk.css"
+  },
+  {
+    "revision": "65aa9f1f19256e0d9c2a",
+    "url": "/project_corona_tracker/static/js/2.83de7107.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/project_corona_tracker/static/js/2.83de7107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1643cf56baa55d0551f",
+    "url": "/project_corona_tracker/static/js/main.ef232db8.chunk.js"
+  },
+  {
+    "revision": "f763dc7a5cb2fcd393f3",
+    "url": "/project_corona_tracker/static/js/runtime-main.7e9d67bc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/project_corona_tracker/static/media/image.d7265326.png"
+  }
+]);
